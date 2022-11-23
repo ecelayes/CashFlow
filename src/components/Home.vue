fb9f3dca810e7df705618bacc6a188ad99942cb3
Emanuel Celayes <ecelayes@gmail.com>
2022-11-23 19:18:39 -0300

    Creacion de Home y Layout

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <Layout>
+        <template #header>
+            <Header />
+        </template>
+        <template #resume>
+            <Resume />
+        </template>
+        <template #movements>
+            <Movements />
+        </template>
+    </Layout>
+</template>
+
+<script>
+import Layout from './Layout.vue'
+import Header from './Header.vue'
+import Resume from './Resume/Index.vue'
+import Movements from './Movements.vue'
+
+export default {
+    components: {
+        Layout,
+        Header,
+        Resume,
+        Movements,
+    },
+}
+</script>
