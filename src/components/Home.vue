fb32800e586ef76f6ab7cd95ffcd6070680a8011
Emanuel Celayes <ecelayes@gmail.com>
2022-11-23 22:57:42 -0300

    Actualizado Index

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,14 @@
             <Header />
         </template>
         <template #resume>
-            <Resume />
+            <Resume
+                :label="'Ahorro Total'"
+                :totalAmount="100000"
+                :amount="1000"
+            >
+                <template #graphic>graphic</template>
+                <template #action>action</template>
+            </Resume>
         </template>
         <template #movements>
             <Movements />
